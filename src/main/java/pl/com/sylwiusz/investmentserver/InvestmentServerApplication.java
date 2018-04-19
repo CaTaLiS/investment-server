24b50dbee74abe285733a9667a900dc7f67bb8e5
Sylwiusz Kosacz <sylwiusz.kosacz@gmail.com>
2018-04-19 19:55:57 +0200

    initial release

---
+++ src/main/java/pl/com/sylwiusz/investmentserver/InvestmentServerApplication.java
@@ -0,0 +1,12 @@
+package pl.com.sylwiusz.investmentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentServerApplication.class, args);
+	}
+}
